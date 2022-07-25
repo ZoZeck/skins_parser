9dcd6c96f6fe597227a620c20484a4c0c4c0f194
sprili <slidedc@gmail.com>
2022-07-25 14:03:13 +0200

    csgo market parser added features

--- Parser/cs_market_parser.py
+++ Parser/cs_market_parser.py
@@ -1,17 +1,48 @@
 import requests
+import json
 from bs4 import BeautifulSoup as BS
+from urllib.parse import unquote
+from collections import OrderedDict
 
-page = 1
 
-while True:
-    r = requests.get('https://market.csgo.com/?t=all&p=' + str(page))
-    html = BS(r.content, 'html.parser')
-    item = html.select('.market-items > .item')
-    if (len(item)):
-        for el in item:
-            title = el.select('.name')
-            price = el.select('.price')
-            mas = print(f'\n{page} page\n{title[0].text}\n{price[0].text} rubles\n')
-        page += 1
-    else:
-        break
+class MarketCSGOObject:
+    def __init__(self, name, link, best_price, iterations):
+        self.name = name
+        self.link= link
+        self.best_price = best_price
+        self.iterations = iterations
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return f'{self.name}, {self.best_price}, https://market.csgo.com{self.link}'
+
+
+def main():
+    page = 0
+    skins_list = dict()
+    while page < 10:
+        r = requests.get(f'https://market.csgo.com/?t=all&p={page}&sd=desc')
+        html = BS(r.content, 'html.parser')
+        item = html.select('.market-items > .item')[:56]
+        if (len(item)):
+            for el in item:
+                link = el.get('href')
+                skin_name = unquote(link.split("-", 2)[2][:-1])
+                skin_price = float(el.select('.price')[0].text.replace(u'\xa0', u' ').replace(" ", ""))
+                if skin_name not in skins_list or skins_list[skin_name].best_price > skin_price:
+                    skins_list[skin_name] = MarketCSGOObject(
+                        skin_name,
+                        link,
+                        skin_price,
+                        skins_list[skin_name].iterations + 1 if skin_name in skins_list else 0
+                    )
+            page += 1
+        else:
+            break
+    return skins_list
+
+
+if __name__ == "__main__":
+    print(main())
